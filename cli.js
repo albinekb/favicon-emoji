4261b0d802670da4dfdb0f7a07216c632ec20374
Albin Ekblom <albinekblom@gmail.com>
2017-03-28 19:27:38 +0200

    🎉 Add readme.md

--- cli.js
+++ cli.js
@@ -1,16 +1,26 @@
 #!/usr/bin/env node
 const args = require('args')
+const opn = require('opn')
 const path = require('path')
 const fsp = require('fs-promise')
 const emojis = require('emoji-img')
 
 args
   .option('emoji', 'choose emoji', 'palm-tree')
-  .option('destination', 'destination', './favicon.ico')
+  .option('destination', 'favicon destination', './favicon.ico')
+  .option('list', 'show list of available emojis')
 
 const flags = args.parse(process.argv)
 
+if (flags.list) {
+  opn('https://www.webpagefx.com/tools/emoji-cheat-sheet/')
+  console.log('🕸 Opened emoji cheat sheet in browser')
+  process.exit(0)
+}
+
 if (flags.emoji && flags.emoji.includes('-')) flags.emoji = flags.emoji.replace(/-/g, '_')
+
+if (flags.emoji && flags.emoji.includes(':')) throw new Error('Please specify emoji without :')
 
 if (!flags.emoji) throw new Error('No emoji specified')
 if (!flags.destination) throw new Error('No destination specified')
@@ -18,7 +28,6 @@ if (!emojis.has(flags.emoji)) throw new Error(`Emoji ${flags.emoji} not found`)
 
 const dest = path.resolve(flags.destination)
 
-console.log(flags.emoji)
 Promise.resolve(emojis.get(flags.emoji))
   .then(path => fsp.copy(path, dest))
   .then(() => console.log('✨ Done, emoji favicon saved to', dest))
---
+++ readme.md
@@ -0,0 +1,42 @@
+# 🌴 favicon-emoji
+
+## Install
+
+### yarn
+```sh
+yarn add global favicon-emoji
+```
+
+### npm
+```sh
+npm install --global favicon-emoji
+```
+
+## Usage
+
+### Generate favicon
+```sh
+favicon-emoji --emoji palm-tree
+
+> ✨ Done, emoji favicon saved to ./favicon.ico
+```
+
+### Help
+
+```sh
+favicon-emoji help
+
+> Usage: favicon emoji [options] [command]
+
+  Commands:
+
+    help  Display help
+
+  Options:
+
+    -d, --destination [value]  favicon destination (defaults to "./favicon.ico")
+    -e, --emoji [value]        choose emoji (defaults to "palm-tree")
+    -h, --help                 Output usage information
+    -l, --list                 show list of available emojis
+    -v, --version              Output the version number
+```
