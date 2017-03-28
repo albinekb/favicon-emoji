ed836cde08fd65d38982d6e45a4a6922955361ea
Albin Ekblom <albinekblom@gmail.com>
2017-03-28 21:07:32 +0200

    🌹 Optimize

--- cli.js
+++ cli.js
@@ -1,8 +1,6 @@
 #!/usr/bin/env node
 const args = require('args')
-const opn = require('opn')
 const path = require('path')
-const fsp = require('fs-promise')
 const emojis = require('emoji-img')
 
 args
@@ -13,7 +11,7 @@ args
 const flags = args.parse(process.argv)
 
 if (flags.list) {
-  opn('https://www.webpagefx.com/tools/emoji-cheat-sheet/')
+  require('opn')('https://www.webpagefx.com/tools/emoji-cheat-sheet/')
   console.log('🕸 Opened emoji cheat sheet in browser')
   process.exit(0)
 }
@@ -25,9 +23,12 @@ if (flags.emoji.includes('-')) flags.emoji = flags.emoji.replace(/-/g, '_')
 if (flags.emoji.includes(':')) throw new Error('Please specify emoji without :')
 if (!emojis.has(flags.emoji)) throw new Error(`Emoji ${flags.emoji} not found`)
 
-const dest = path.resolve(flags.destination)
+if (flags.emoji && flags.destination) {
+  const dest = path.resolve(flags.destination)
+  const fsp = require('fs-promise')
 
-Promise.resolve(emojis.get(flags.emoji))
+  Promise.resolve(emojis.get(flags.emoji))
   .then(path => fsp.copy(path, dest))
   .then(() => console.log('✨ Done, emoji favicon saved to', dest))
   .catch(error => console.error('error', error))
+}
